7357a7df0eddd1208dc3b2a62cf189e780d5ea50
ili <s2000916@edu.bc.fi>
2022-11-18 14:55:01 +0200

    kysytään käyttäjältä summa vai erotus
    kysytään käyttäjältä summa vai erotus

--- GitCalculator/Program.cs
+++ GitCalculator/Program.cs
@@ -11,12 +11,21 @@ namespace GitCalculator
             
             Console.WriteLine("Anna toinen numero");
             int toka = Convert.ToInt32(Console.ReadLine());
-            
-            Console.WriteLine("Summa:");
-            Console.WriteLine(eka + toka);
-            
-            Console.WriteLine("Erotus:");
-            Console.WriteLine(eka - toka);
+
+
+            Console.WriteLine("Kumman haluat tulostaa, 1: Summan vai 2: Erotuksen?");
+            int kolmas = Convert.ToInt32(Console.ReadLine());
+
+           if (kolmas == 1)
+            {
+                Console.WriteLine("Summa:");
+                Console.WriteLine(eka + toka);
+            }
+           if (kolmas == 2)
+            {
+                Console.WriteLine("Erotus:");
+                Console.WriteLine(eka - toka);
+            }
         }
     }
 }

0de495783e53625d26be312902007d02687360d8
ili <s2000916@edu.bc.fi>
2022-11-18 15:29:14 +0200

    Kaikki metodit
    Vaihettu jokainen laskutapa metodiksi

--- GitCalculator/Program.cs
+++ GitCalculator/Program.cs
@@ -6,16 +6,33 @@ namespace GitCalculator
     {
         static void Main(string[] args)
         {
-            Console.Write("Anna ensimmäinen numero: ");
+            Console.WriteLine("Anna kaksi lukua");
             int eka = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Anna toinen numero: ");
             int toka = Convert.ToInt32(Console.ReadLine());
-            yhteenlasku(eka, toka);
-            Console.ReadLine();
+            int vastaus1 = Summa(eka, toka);
+            int vastaus2 = Erotus(eka, toka);
+            int vastaus3 = Kerto(eka, toka);
+            double vastaus4 = Jako(eka, toka);
+            Console.WriteLine("Summa: {0}\n" + "Erotus: {1} \n" + "Kerto: {2} \n" + "Jako: {3} \n", vastaus1, vastaus2, vastaus3, vastaus4);
         }
-        public static void yhteenlasku(int eka, int toka)
+
+        public static int Summa(int eka, int toka)
         {
-            Console.WriteLine(eka + toka);
+            return eka + toka;
         }
+
+        public static int Erotus(int eka, int toka)
+        {
+            return eka - toka;
+        }
+        public static int Kerto(int eka, int toka)
+        {
+            return eka * toka;
+        }
+        public static double Jako(int eka, int toka)
+        {
+            return eka / toka;
+        }
+
     }
 }
